10cb544d1464ce9d1c13d58ae834d938480265d4
muhammadfahrurriza <muhammadfahrurriza@students.undip.ac.id>
2025-04-30 13:43:02 +0700

    fix seed

--- database/seeders/ModelHasRolesSeeder.php
+++ database/seeders/ModelHasRolesSeeder.php
@@ -14,15 +14,13 @@ class ModelHasRolesSeeder extends Seeder
     public function run(): void
     {
         $modelRoles = [
-            ['role_id' => 1, 'model_type' => 'App\\Models\\User', 'model_id' => 2],
-            ['role_id' => 1, 'model_type' => 'App\\Models\\User', 'model_id' => 9],
-            ['role_id' => 2, 'model_type' => 'App\\Models\\User', 'model_id' => 1],
-            ['role_id' => 2, 'model_type' => 'App\\Models\\User', 'model_id' => 3],
-            ['role_id' => 3, 'model_type' => 'App\\Models\\User', 'model_id' => 4],
-            ['role_id' => 4, 'model_type' => 'App\\Models\\User', 'model_id' => 7],
-            ['role_id' => 5, 'model_type' => 'App\\Models\\User', 'model_id' => 8],
-            ['role_id' => 6, 'model_type' => 'App\\Models\\User', 'model_id' => 5],
-            ['role_id' => 7, 'model_type' => 'App\\Models\\User', 'model_id' => 6],
+            ['role_id' => 1, 'model_type' => 'App\\Models\\User', 'model_id' => 1],
+            ['role_id' => 2, 'model_type' => 'App\\Models\\User', 'model_id' => 2],
+            ['role_id' => 3, 'model_type' => 'App\\Models\\User', 'model_id' => 3],
+            ['role_id' => 4, 'model_type' => 'App\\Models\\User', 'model_id' => 4],
+            ['role_id' => 5, 'model_type' => 'App\\Models\\User', 'model_id' => 5],
+            ['role_id' => 6, 'model_type' => 'App\\Models\\User', 'model_id' => 6],
+            ['role_id' => 7, 'model_type' => 'App\\Models\\User', 'model_id' => 7],
         ];
 
         DB::table('model_has_roles')->insertOrIgnore($modelRoles);
--- database/seeders/RoleHasPermissionSeeder.php
+++ database/seeders/RoleHasPermissionSeeder.php
@@ -19,6 +19,15 @@ class RoleHasPermissionSeeder extends Seeder
             ['permission_id' => 1, 'role_id' => 3],
             ['permission_id' => 1, 'role_id' => 4],
             ['permission_id' => 1, 'role_id' => 5],
+            ['permission_id' => 1, 'role_id' => 6],
+            ['permission_id' => 1, 'role_id' => 7],
+            ['permission_id' => 2, 'role_id' => 1],
+            ['permission_id' => 2, 'role_id' => 2],
+            ['permission_id' => 2, 'role_id' => 3],
+            ['permission_id' => 2, 'role_id' => 4],
+            ['permission_id' => 2, 'role_id' => 5],
+            ['permission_id' => 2, 'role_id' => 6],
+            ['permission_id' => 2, 'role_id' => 7],
         ];
 
         DB::table('role_has_permissions')->insertOrIgnore($rolePermissions);
